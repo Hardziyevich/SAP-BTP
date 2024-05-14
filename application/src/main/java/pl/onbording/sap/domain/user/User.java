a87dfe7aa2d18728ce7868b07aa35060b4d9ae21
pavelhardziyevich <pashagordievich@gmail.com>
2024-05-14 16:06:26 +0200

    add missing @Table

--- application/src/main/java/pl/onbording/sap/domain/user/User.java
+++ application/src/main/java/pl/onbording/sap/domain/user/User.java
@@ -1,9 +1,6 @@
 package pl.onbording.sap.domain.user;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,6 +11,7 @@ import java.io.Serializable;
 @Getter
 @Setter
 @NoArgsConstructor
+@Table(name = "users")
 public class User implements Serializable {
 
     @Id
